5b10dd6a2b64b8e1d5e4dd48c1cb7adffac4646c
ilyass atlassi <atlassiilyass@gmail.com>
2023-06-27 17:25:42 +0100

    Add relation betwen table

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,12 @@
 Rails.application.routes.draw do
-  resources :recipe_foods
-  resources :foods
-  resources :recipes
+  # resources :recipe_foods
+  # resources :foods
+  # resources :recipes
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+
+  root 'foods#index'
+  resources :foods , only: %i[index show]
 
   # Defines the root path route ("/")
   # root "articles#index"
