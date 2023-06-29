d7769523c2edf910bb31a04dad186f2c352918af
Abe1able <abel.workmail@gmail.com>
2023-06-29 09:35:34 +0300

    Add show method for recipes controller

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -7,6 +7,8 @@ class RecipesController < ApplicationController
 
   # GET /recipes/1 or /recipes/1.json
   def show
+    @recipe = Recipe.find(params[:id])
+    @recipe_foods = @recipe.recipe_foods.includes([:food])
   end
 
   # GET /recipes/new
