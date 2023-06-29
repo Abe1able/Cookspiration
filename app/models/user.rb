8e5878c511f29da872a5203bfd899f0e42022493
Abe1able <abel.workmail@gmail.com>
2023-06-29 20:58:52 +0300

    Add role to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :foods, foreign_key: 'user_id', dependent: :destroy
-  has_many :recipes, foreign_key: 'user_id' dependent: :destroy
+  has_many :recipes, foreign_key: 'user_id', dependent: :destroy
   validates :name, presence: true
 
   def admin?
