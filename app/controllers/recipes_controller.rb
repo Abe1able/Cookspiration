d053afc0ce6cbe21b5af57d96aef6842cbff208f
ilyass atlassi <atlassiilyass@gmail.com>
2023-06-30 17:51:36 +0100

    add shopping_list

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -44,6 +44,30 @@ class RecipesController < ApplicationController
     redirect_to shopping_list_path(params[:recipe_id])
   end
 
+  def shopping_list
+    @quantity = []
+    @foods = []
+
+    recipe_foods = RecipeFood.where(recipe_id: params[:recipe_id])
+    inventory = Inventory.find(params[:inventory_id])
+
+    recipe_foods.each do |recipe_food|
+      food = recipe_food.food
+      inventory_food = inventory.inventory_foods.find_by(food_id: food.id)
+
+      next unless inventory_food.nil? || inventory_food.quantity < recipe_food.quantity
+
+      missing_quantity = inventory_food.nil? ? recipe_food.quantity : recipe_food.quantity - inventory_food.quantity
+      @foods << food.name
+      @quantity << [missing_quantity, food.price]
+    end
+
+    @total = 0
+    @quantity.each do |q|
+      @total += q[0].to_i * q[1].to_i
+    end
+  end
+
   private
     def params_recipe
       params.require(:recipe).permit(:name, :preparation_time, :description, :public, :cooking_time)
